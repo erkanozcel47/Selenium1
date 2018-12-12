2d687e83af3594d49af4e795bb4389d003e042be
erkanozcel47 <erkanozcelik507@gmail.com>
2018-12-11 22:20:43 -0500

    This is my first Selenium prject

---
+++ src/com/cbt/SeleniumDemo.java
@@ -0,0 +1,22 @@
+ package com.cbt;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumDemo {
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\erkan\\OneDrive\\Masaüstü\\Selenium\\drivers\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://google.com");
+		
+		
+		
+		
+		
+	}
+
+}
